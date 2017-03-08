32e092c71f3b2cb86173a23224ec92c3fa1e1a6d
Bobby Lei <bobl53@gmail.com>
2017-03-08 21:23:15 +0800

    sending a ping from server to client

--- web/static/js/video.js
+++ web/static/js/video.js
@@ -19,6 +19,12 @@ let Video = {
 
     let vidChannel = socket.channel("videos:" + videoId)
 
+    // {count} is the ES6 object destructing feature
+    // e.g.
+    // var o = {p: 42, q: true}
+    // var {p, q} = o
+    vidChannel.on("ping", ({count}) => console.log("PING", count))
+
     vidChannel.join()
       .receive("ok", resp => console.log("joined the video channel", resp))
       .receive("error", reason => console.log("join failed", reason))
