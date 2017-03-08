400243c87048374d9090b723b2267ae3497e9d4b
Bobby Lei <bobl53@gmail.com>
2017-03-08 21:12:37 +0800

    joining the channel from the js code

--- web/static/js/video.js
+++ web/static/js/video.js
@@ -18,6 +18,10 @@ let Video = {
     let postButton = document.getElementById("msg-submit")
 
     let vidChannel = socket.channel("videos:" + videoId)
+
+    vidChannel.join()
+      .receive("ok", resp => console.log("joined the video channel", resp))
+      .receive("error", reason => console.log("join failed", reason))
   }
 }
 
