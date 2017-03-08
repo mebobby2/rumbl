6dc3d32e910a721848c4de72fb7955110aeb4fc0
Bobby Lei <bobl53@gmail.com>
2017-03-08 22:57:32 +0800

    the video channel is accepting messages

--- web/channels/video_channel.ex
+++ web/channels/video_channel.ex
@@ -7,6 +7,16 @@ defmodule Rumbl.VideoChannel do
     {:ok, socket}
   end
 
+  def handle_in("new_annotation", params, socket) do
+    broadcast! socket, "new_annotation", %{
+      user: %{username: "anon"},
+      body: params["body"],
+      at: params["at"]
+    }
+
+    {:reply, :ok, socket}
+  end
+
   def handle_info(:ping, socket) do
     count = socket.assigns[:count] || 1
     push socket, "ping", %{count: count}
